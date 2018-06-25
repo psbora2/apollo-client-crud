bcb9b3f6d9be81fa7df7c8efaf18c2d5a9f4b7ac
psbora2 <pawansinghbora4@gmail.com>
2018-06-25 21:06:09 +0530

    client related changes

--- src/store/client.js
+++ src/store/client.js
@@ -1,13 +1,29 @@
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import ApolloClient from 'apollo-client'
+import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost';
+
+const httpLink = new HttpLink({ uri: 'http://13.127.67.179/graphql' });
+
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from local storage.
+  const token = localStorage.getItem('access-token');
+	const uid = localStorage.getItem('uid');
+	const client = localStorage.getItem('client');
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      'access-token': token ? `${token}` : '',
+      'uid': uid ? `${uid}` : '',
+      'client': client ? `${client}` : ''
+    }
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
+});
 
 const client = new ApolloClient({
-  link: createHttpLink({
-    uri: "/graphql/",
-    headers: {'Access-Control-Allow-Origin':'*'}
-  }),
-  cache: new InMemoryCache(),
-})
+  link: authLink.concat(httpLink), // Chain it with the HttpLink
+  cache: new InMemoryCache()
+});
 
-export default client
+export default client; 
